be55d9b9d9435a32ecd2ff2cc8898ab6026e83e0
Jakex10sen <jakex10sen@gmail.com>
2014-11-17 18:25:24 -0500

    CSV Import
    now adds the imported file to  /imports and prints content to screen

--- import.php
+++ import.php
@@ -26,15 +26,15 @@ if ($uploadOk == 0) {
     }
 }
 
-
-
-
-
-/*
 $row = 1;
-if (($handle = fopen("test.csv ", "r ")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ", ")) !== FALSE) {
+if (($handle = fopen($target_file, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
-        echo "<p>$num fields in line $row:
-<br />
-</p>\n"; $row++; for ($c=0; $c< $num; $c++) { echo $data[$c] . "<br />\n"; } } fclose($handle); } */
+        echo "<p>$num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c< $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+} 
